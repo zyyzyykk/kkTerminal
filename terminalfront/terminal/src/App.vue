f8cec76e7cff458d35c8113ec6aada586191ab5c
zyyzyykk <zyyzyykk@126.com>
2023-09-05 01:15:14 +0800

    feat: initial model

---
+++ terminalfront/terminal/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <FrameWork></FrameWork>
+</template>
+
+<script>
+import FrameWork from "./views/FrameWork";
+
+export default {
+  name: 'App',
+  components: {
+    FrameWork,
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ terminalfront/terminal/src/Utils/Loading.js
@@ -0,0 +1,11 @@
+// 加载中。。。
+import { ElLoading } from 'element-plus'
+export default function() {
+    const loading = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        // background: 'rgba(0, 0, 0, 0.7)',
+    })
+
+    return loading;
+}
