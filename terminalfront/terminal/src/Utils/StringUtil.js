c5ee132322f56a187b5dede30fd97dde766db4de
zyyzyykk <zyyzyykk@126.com>
2023-09-08 01:50:51 +0800

    成功解析ls的颜色

---
+++ terminalfront/terminal/src/Utils/StringUtil.js
@@ -0,0 +1,12 @@
+// 生成随机字符串
+export function generateRandomString(length) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
