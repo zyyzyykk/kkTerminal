4569a3a4898a7b5682bc1a888f40c6be5d25c441
zyyzyykk <zyyzyykk@126.com>
2023-09-09 01:06:31 +0800

    feat: connection failure displays configuration memory

--- terminalfront/terminal/src/Utils/HandleANSI.js
+++ terminalfront/terminal/src/Utils/HandleANSI.js
@@ -4,6 +4,8 @@ import { AnsiUp } from 'ansi-up';
 let ansi_up = new AnsiUp();
 
 const parseShellToHtml = (stream) => {
+  // \x1B[0m\x1B[01;34muuu\x1B[0m\r
+  // \u001b[0m\u001b[01;34muuu\u001b[0m\r
   return ansi_up.ansi_to_html(stream)
 }
 
