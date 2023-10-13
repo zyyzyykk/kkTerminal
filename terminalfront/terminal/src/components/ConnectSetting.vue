f0c84724e2e32692270fa7caa1455c744e5ca5fb
zyyzyykk <zyyzyykk@126.com>
2023-10-13 17:17:00 +0800

    release: 1.0

--- terminalfront/terminal/src/components/ConnectSetting.vue
+++ terminalfront/terminal/src/components/ConnectSetting.vue
@@ -1,39 +1,60 @@
 <template>
   <el-dialog
     v-model="DialogVisilble"
-    :before-close="closeDialog"
+    destroy-on-close
+    :width="360"
     title="连接设置"
-    width="30%"
+    :modal="false"
+    draggable
   >
     <div style="margin-top: -15px;"></div>
-    <div class="item-class">
-      <div style="display: flex; align-items: center; margin-bottom: 15px;">
+    <div>
+      <div class="item-class" style="margin-bottom: 15px;">
+        <div>主机ip：</div>
+        <div>
+          <el-input v-model="setInfo.server_ip" class="w-50 m-2" placeholder="输入主机ip">
+            <template #prefix>
+              <el-icon><HomeFilled /></el-icon>
+            </template>
+          </el-input>
+        </div>
+      </div>
+      <div class="item-class" style="margin-bottom: 15px;">
+        <div>端口号：</div>
+        <div>
+          <el-input v-model="setInfo.server_port" class="w-50 m-2" placeholder="输入端口号">
+            <template #prefix>
+              <el-icon><Paperclip /></el-icon>
+            </template>
+          </el-input>
+        </div>
+      </div>
+      <div class="item-class" style="margin-bottom: 15px;">
         <div>用户名：</div>
         <div>
-          <el-input v-model="setInfo.user_name" class="w-50 m-2" placeholder="请输入用户名">
+          <el-input v-model="setInfo.server_user" class="w-50 m-2" placeholder="输入用户名">
             <template #prefix>
               <el-icon class="el-input__icon"><User /></el-icon>
             </template>
           </el-input>
         </div>
       </div>
-      <div style="display: flex; align-items: center; margin-bottom: 5px;">
+      <div class="item-class" style="margin-bottom: 5px;">
         <div>密 &nbsp; 码：</div>
         <div>
-          <el-input v-model="setInfo.password" show-password class="w-50 m-2" placeholder="请输入密码">
+          <el-input v-model="setInfo.server_password" type="password" class="w-50 m-2" placeholder="输入密码">
             <template #prefix>
               <el-icon class="el-input__icon"><Lock /></el-icon>
             </template>
           </el-input>
         </div>
       </div>
-      <div><el-checkbox class="a-link" v-model="setInfo.isRemember">记住</el-checkbox></div>
     </div>
     <div class="errInfo"> {{ err_msg }} </div>
-    <div style="margin-bottom: 15px;"></div>
+    <div style="margin-bottom: 5px;"></div>
     <div style="display: flex; border-top: 1px solid #f1f2f4;">
       <div style="flex: 1;"></div>
-      <el-button type="primary" @click="setSSH" style="margin-bottom: -15px; margin-top: 10px;">
+      <el-button size="small" type="primary" @click="confirm" style="margin-bottom: -15px; margin-top: 10px;">
         确定
       </el-button>
     </div>
@@ -42,12 +63,12 @@
 
 <script>
 import { ref } from 'vue';
-import { Base64 } from '../Utils/Base64Util';
 
 export default {
   name:'ConnectSetting',
   components: {
   },
+  props:['env'],
   setup(props,context) {
 
     // 控制Dialog显示
@@ -55,36 +76,22 @@ export default {
     const err_msg = ref('');
 
     // 设置信息
-    const setInfo = ref({});
-    if(localStorage.getItem('ssh')) {
-      setInfo.value = {...JSON.parse(Base64.decode(localStorage.getItem('ssh')))};
-    }
-    const setSSH = () => {
-      // 校验参数
-      if(!(setInfo.value.user_name && setInfo.value.user_name != '')) {
-        err_msg.value = '用户名不能为空';
-        return;
-      }
-      if(!(setInfo.value.password && setInfo.value.password != '')) {
-        err_msg.value = '密码不能为空';
-        return;
-      }
-      context.emit('doSSHCheck',setInfo.value);
-    }
-
-    // 关闭
-    const closeDialog = (done) => {
-      err_msg.value = '';
-      setInfo.value = {};
-      done();
+    const setInfo = ref({
+      server_ip: props.env.server_ip,
+      server_port: props.env.server_port,
+      server_user: props.env.server_user,
+      server_password:props.env.server_password,
+    });
+    const confirm = () => {
+      context.emit('callback',setInfo.value);
+      DialogVisilble.value = false;
     }
 
     return {
+      setInfo,
       DialogVisilble,
       err_msg,
-      closeDialog,
-      setSSH,
-      setInfo,
+      confirm,
 
     }
   }
@@ -94,6 +101,12 @@ export default {
 </script>
 
 <style scoped>
+
+.item-class {
+  display: flex; 
+  align-items: center; 
+}
+
 .errInfo{
   font-size: 12px;
   color: rgb(234, 80, 80);
