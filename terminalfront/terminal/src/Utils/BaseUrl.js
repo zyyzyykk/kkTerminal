23a7a0c0fd2e4c6fc7263989a40799244fd398f6
zyyzyykk <zyyzyykk@126.com>
2023-10-14 18:49:12 +0800

    正式部署1.0

--- terminalfront/terminal/src/Utils/BaseUrl.js
+++ terminalfront/terminal/src/Utils/BaseUrl.js
@@ -1,2 +1,6 @@
-const base_url = 'ws://' + window.location.host + '/socket/ssh/';
+let http_protocol = window.location.protocol;
+let ws_protocol = '';
+if(http_protocol == 'https:') ws_protocol = 'wss://';
+else ws_protocol = 'ws://';
+const base_url = ws_protocol + window.location.host + '/socket/ssh/';
 export default base_url;
