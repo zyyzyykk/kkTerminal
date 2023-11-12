c5c90107a5c12a733c18fa281bf194453403dd53
kk <wangjingkk>
2023-11-12 23:42:23 +0800

    准备更新虚拟终端大小自适应

--- terminalfront/terminal/src/Utils/BaseUrl.js
+++ terminalfront/terminal/src/Utils/BaseUrl.js
@@ -1,11 +1,11 @@
 // 上线环境
-let http_protocol = window.location.protocol;
-let ws_protocol = '';
-if(http_protocol == 'https:') ws_protocol = 'wss://';
-else ws_protocol = 'ws://';
-const base_url = ws_protocol + window.location.host + '/socket/ssh/';
+// let http_protocol = window.location.protocol;
+// let ws_protocol = '';
+// if(http_protocol == 'https:') ws_protocol = 'wss://';
+// else ws_protocol = 'ws://';
+// const base_url = ws_protocol + window.location.host + '/socket/ssh/';
 
 // 本地开发环境
-// const base_url = 'ws://localhost:3000/socket/ssh/';
+const base_url = 'ws://localhost:3000/socket/ssh/';
 
 export default base_url;
