8f5ea06e3b14b997c2893a438507635acbcbaaf5
kk <wangjingkk>
2023-12-12 16:27:04 +0800

    2.8.0文件管理

--- terminalBackend/terminal/src/main/java/com/kkbpro/terminal/Config/WebSocketConfig.java
+++ terminalBackend/terminal/src/main/java/com/kkbpro/terminal/Config/WebSocketConfig.java
@@ -3,10 +3,12 @@ package com.kkbpro.terminal.Config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebSocketConfig {
-
     @Bean
     public ServerEndpointExporter serverEndpointExporter() {
         return new ServerEndpointExporter();
