41908d726ed2ea8bd83e0223f26c665fe4b0dde2
kk <wangjingkk>
2024-01-03 15:58:44 +0800

    禁止图标拖拽

--- terminalfront/terminal/src/App.vue
+++ terminalfront/terminal/src/App.vue
@@ -14,4 +14,11 @@ export default {
 </script>
 
 <style>
+/* 禁止图片拖拽 */
+img {
+  -webkit-user-drag: none; /* Safari */
+  -khtml-user-drag: none; /* Konqueror HTML */
+  -moz-user-drag: none; /* Firefox */
+  -o-user-drag: none; /* Opera */
+}
 </style>
