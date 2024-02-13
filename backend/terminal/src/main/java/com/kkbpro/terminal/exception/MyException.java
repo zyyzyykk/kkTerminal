a0b66b11b6e66021ad1693485e963f815d14d3f5
“zyyzyykk” <zyyzyy0805@163.com>
2024-02-13 15:30:17 +0800

    feat: menu display

--- backend/terminal/src/main/java/com/kkbpro/terminal/exception/MyException.java
+++ backend/terminal/src/main/java/com/kkbpro/terminal/exception/MyException.java
@@ -1,17 +1,17 @@
-package com.kkbpro.terminal.exceptionHandler;
+package com.kkbpro.terminal.exception;
 
 
 import com.kkbpro.terminal.result.Result;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * 自定义异常，解决参数校验问题
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class MyException extends RuntimeException{
-
     private Result result;
-
     public MyException(String message){
         super(message);
     }
