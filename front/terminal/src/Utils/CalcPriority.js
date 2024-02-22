0317c40e89b39a5a9222d92c68a2028cc93af1d9
“zyyzyykk” <zyyzyy0805@163.com>
2024-02-22 18:08:08 +0800

    修复部分文件权限属性错误的bug

--- front/terminal/src/Utils/CalcPriority.js
+++ front/terminal/src/Utils/CalcPriority.js
@@ -29,7 +29,11 @@ export const calcPriority = (type,attrs) => {
         let attr = attrs[i];
         let role = priorityDic[attr[0]];
         let name = priorityDic[attr[4]];
-        rattrs[role*3+name] = attr[4].toLowerCase();
+        if(role != null && role != undefined 
+          && name != null && name != undefined 
+          && attr[4] != null && attr[4] != undefined) {
+            rattrs[role*3+name] = attr[4].toLowerCase();
+        }
     }
     for(let i=0;i<rattrs.length;i++) rtype += rattrs[i];
 
