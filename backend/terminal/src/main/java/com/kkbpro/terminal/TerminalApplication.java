f7edc602184db69ad72eec3cb6ecfed1aa6ff5cc
zyyzyykk <zyyzyykk@126.com>
2024-07-26 02:12:33 +0800

    feat: open PC window

--- backend/terminal/src/main/java/com/kkbpro/terminal/TerminalApplication.java
+++ backend/terminal/src/main/java/com/kkbpro/terminal/TerminalApplication.java
@@ -3,11 +3,34 @@ package com.kkbpro.terminal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.awt.*;
+import java.net.URI;
+
 @SpringBootApplication
 public class TerminalApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TerminalApplication.class, args);
+        // 支持PC端：Windows和Mac
+        String osName = System.getProperty("os.name").toLowerCase();
+        if(osName.contains("win") || osName.contains("mac")) {
+            // 打开Web浏览器
+            openWebPage("http://localhost:3000");
+        }
+    }
+
+    private static void openWebPage(String url) {
+        if (Desktop.isDesktopSupported()) {
+            try {
+                Desktop.getDesktop().browse(new URI(url));
+            } catch (Exception e) {
+                System.err.println("Open browser error.");
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Cannot open browser automatically.");
+        }
     }
 
 }
