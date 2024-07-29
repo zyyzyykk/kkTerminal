e7a93a70416da1199a2b8d07ae3004801f71ca78
“zyyzyykk” <zyyzyykk@126.com>
2024-07-30 01:10:03 +0800

    功能TCode位置更改&本地部署打开窗口

--- backend/terminal/src/main/java/com/kkbpro/terminal/TerminalApplication.java
+++ backend/terminal/src/main/java/com/kkbpro/terminal/TerminalApplication.java
@@ -3,33 +3,47 @@ package com.kkbpro.terminal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.awt.*;
-import java.net.URI;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @SpringBootApplication
 public class TerminalApplication {
-
     public static void main(String[] args) {
-
+        // 打开Web浏览器
+        openWebPage();
         SpringApplication.run(TerminalApplication.class, args);
+    }
+
+    private static void openWebPage() {
         // 支持PC端：Windows和Mac
-        String osName = System.getProperty("os.name").toLowerCase();
-        if(osName.contains("win") || osName.contains("mac")) {
-            // 打开Web浏览器
-            openWebPage("http://localhost:3000");
+        String os = System.getProperty("os.name").toLowerCase();
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            // e.g. mac os x
+            if (os.contains("mac")) {
+                runtime.exec("open " + getUrl());
+            }
+            // e.g. windows 10
+            else if (os.contains("win")) {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + getUrl());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static void openWebPage(String url) {
-        if (Desktop.isDesktopSupported()) {
-            try {
-                Desktop.getDesktop().browse(new URI(url));
-            } catch (Exception e) {
-                System.err.println("Open browser error.");
-                e.printStackTrace();
-            }
-        } else {
-            System.err.println("Cannot open browser automatically.");
+
+    private static String getUrl() {
+        String baseUrl = "http://localhost:";
+        Properties properties = new Properties();
+        InputStream inputStream = TerminalApplication.class.getClassLoader().getResourceAsStream("application.properties");
+        try {
+            properties.load(inputStream);
+            return baseUrl + properties.getProperty("server.port");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return baseUrl + "3000";
         }
     }
 
