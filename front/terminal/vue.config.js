8028c9df71c229b74113f13e868ddb876bb87df9
“zyyzyykk” <zyyzyykk@126.com>
2025-02-26 22:32:16 +0800

    perf: optimize packaging volume

--- front/terminal/vue.config.js
+++ front/terminal/vue.config.js
@@ -2,4 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  productionSourceMap: false,     // 关闭源映射，减小打包体积，避免暴露源代码
 });
